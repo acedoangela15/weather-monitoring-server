790e18dd20ad20f60162b0e1eb77f489b4d2546a
Renz <77318199+renzoyyan@users.noreply.github.com>
2023-11-24 15:52:05 +0800

    refactor route and controller

--- routes/log-routes.js
+++ routes/log-routes.js
@@ -1,10 +1,10 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const logController = require("../controllers/log-controller");
+const logController = require('../controllers/log-controller');
 
-router.post("/new", logController.createLog);
-router.get("/", logController.getLatestLog);
-router.put("/:uid", logController.updateLog);
+router.post('/new', logController.createLog);
+router.get('/', logController.getLatestLog);
+router.post('/update', logController.updateLog);
 
 module.exports = router;
